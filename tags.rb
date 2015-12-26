ca1aa5485c98251975cb649f5496c0f12b299ed2
在望もむ <momu@ll-x-ll.me>
2015-12-26 21:45:24 +0900

    とりあえず作成したものをコミット。

---
+++ tags.rb
@@ -0,0 +1,72 @@
+module Jekyll
+    class TagPage < Page
+        def initialize(site, base, dir, tag)
+            @site = site
+            @base = base
+            @dir  = dir
+            @name = 'index.html'
+            self.process(name)
+            
+            self.read_yaml(File.join(base, '_layouts'), 'tag.html')
+            self.data['title'] = "Tag:#{tag}"
+            self.data['posts'] = site.tags[tag]
+            self.data['title_detail'] = 'タグ「' + tag + '」' + 'がつけられた記事'
+        end
+    end
+    
+    class TagPageGenerator < Generator
+        
+        safe true
+        
+        def generate(site)
+            site.tags.each_key do |tag|
+                site.pages << TagPage.new(site, site.source, File.join('tags', tag), tag)
+            end
+        end
+    end
+    
+    class TagCloud < Liquid::Tag
+
+        def initialize(tag_name, text, tokens)
+            super
+        end
+
+        def render(context)
+            tag_array = []
+            site = context.registers[:site]
+            site.tags.each do |tag, tag_pages|
+                tag_array << tag
+            end
+            tag_array.sort!
+
+            tagcloud = "<ul>"
+            tag_array.each do |tag|
+                tagcloud << "<li><a href='#{site.baseurl}/tags/#{tag}/index.html'>#{tag}</a></li>"
+            end
+            tagcloud << "</ul>"
+            "#{tagcloud}"
+        end
+    end
+
+    class TagCloudPage < Page
+        def initialize(site, base, dir)
+            @site = site
+            @base = base
+            @dir  = dir
+            @name = 'index.html'
+            self.process(name)
+            self.read_yaml(File.join(base, '_layouts'), 'tag_list.html')
+            self.data['title'] = "タグ一覧"
+            self.data['posts'] = site.documents
+        end
+    end
+
+    class TagCloudPageGenerator < Generator
+        safe true
+        def generate(site)
+            site.pages << TagCloudPage.new(site, site.source, 'tag_list')
+        end
+    end
+end
+
+Liquid::Template.register_tag('tag_cloud', Jekyll::TagCloud)
