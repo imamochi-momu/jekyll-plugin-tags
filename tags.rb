f4ce4b1bf0bcb039caad952a76615d2433909337
在望もむ <momu@ll-x-ll.me>
2016-07-05 12:05:48 +0900

    ・インデントを見直し。 ・記事数を追加。

--- tags.rb
+++ tags.rb
@@ -1,72 +1,71 @@
 module Jekyll
-    class TagPage < Page
-        def initialize(site, base, dir, tag)
-            @site = site
-            @base = base
-            @dir  = dir
-            @name = 'index.html'
-            self.process(name)
-            
-            self.read_yaml(File.join(base, '_layouts'), 'tag.html')
-            self.data['title'] = "Tag:#{tag}"
-            self.data['posts'] = site.tags[tag]
-            self.data['title_detail'] = 'タグ「' + tag + '」' + 'がつけられた記事'
-        end
+  class TagPage < Page
+    def initialize(site, base, dir, tag)
+      @site = site
+      @base = base
+      @dir  = dir
+      @name = 'index.html'
+      self.process(name)
+
+      self.read_yaml(File.join(base, '_layouts'), 'tag.html')
+      self.data['title'] = "Tag:#{tag}"
+      self.data['posts'] = site.tags[tag]
+      self.data['title_detail'] = 'タグ「' + tag + '」' + 'がつけられた記事'
     end
-    
-    class TagPageGenerator < Generator
-        
-        safe true
-        
-        def generate(site)
-            site.tags.each_key do |tag|
-                site.pages << TagPage.new(site, site.source, File.join('tags', tag), tag)
-            end
-        end
+  end
+
+  class TagPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      site.tags.each_key do |tag|
+        site.pages << TagPage.new(site, site.source, File.join('tags', tag), tag)
+      end
     end
-    
-    class TagCloud < Liquid::Tag
+  end
 
-        def initialize(tag_name, text, tokens)
-            super
-        end
+  class TagCloud < Liquid::Tag
 
-        def render(context)
-            tag_array = []
-            site = context.registers[:site]
-            site.tags.each do |tag, tag_pages|
-                tag_array << tag
-            end
-            tag_array.sort!
-
-            tagcloud = "<ul>"
-            tag_array.each do |tag|
-                tagcloud << "<li><a href='#{site.baseurl}/tags/#{tag}/index.html'>#{tag}</a></li>"
-            end
-            tagcloud << "</ul>"
-            "#{tagcloud}"
-        end
+    def initialize(tag_name, text, tokens)
+      super
     end
 
-    class TagCloudPage < Page
-        def initialize(site, base, dir)
-            @site = site
-            @base = base
-            @dir  = dir
-            @name = 'index.html'
-            self.process(name)
-            self.read_yaml(File.join(base, '_layouts'), 'tag_list.html')
-            self.data['title'] = "タグ一覧"
-            self.data['posts'] = site.documents
-        end
-    end
+    def render(context)
+      tag_array = Array.new(0)
+      site = context.registers[:site]
+      site.tags.each do |tag, tag_pages|
+        tag_array << {:title => tag, :count => tag_pages.count}
+      end
+      tag_array.sort_by!{|item| -item[:count]}
 
-    class TagCloudPageGenerator < Generator
-        safe true
-        def generate(site)
-            site.pages << TagCloudPage.new(site, site.source, 'tag_list')
-        end
+      tagcloud = "<ul>"
+      tag_array.each do |tag|
+        tagcloud << "<li><a href='#{site.baseurl}/tags/#{tag[:title]}/index.html'>#{tag[:title]} (#{tag[:count]})</a></li>"
+      end
+      tagcloud << "</ul>"
+      "#{tagcloud}"
     end
+  end
+
+  class TagCloudPage < Page
+    def initialize(site, base, dir)
+      @site = site
+      @base = base
+      @dir  = dir
+      @name = 'index.html'
+      self.process(name)
+      self.read_yaml(File.join(base, '_layouts'), 'tag_list.html')
+      self.data['title'] = "タグ一覧"
+      self.data['posts'] = site.documents
+    end
+  end
+
+  class TagCloudPageGenerator < Generator
+    safe true
+    def generate(site)
+      site.pages << TagCloudPage.new(site, site.source, 'tag_list')
+    end
+  end
 end
 
 Liquid::Template.register_tag('tag_cloud', Jekyll::TagCloud)
